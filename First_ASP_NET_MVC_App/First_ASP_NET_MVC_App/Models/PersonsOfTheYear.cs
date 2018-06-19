2353bd889707cbc613f5d52c795a826ca4ddeb14
paulritzman <ritzman.paul@gmail.com>
2018-06-19 12:59:53 -0700

    Added Models Class and started constructing routes

---
+++ First_ASP_NET_MVC_App/First_ASP_NET_MVC_App/Models/PersonsOfTheYear.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace First_ASP_NET_MVC_App.Models
+{
+    public class PersonsOfTheYear
+    {
+        public int Year { get; set; }
+        public string Honor { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int Birth_Year { get; set; }
+        public int DeathYear { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Context { get; set; }
+    }
+}
--- First_ASP_NET_MVC_App/First_ASP_NET_MVC_App/Startup.cs
+++ First_ASP_NET_MVC_App/First_ASP_NET_MVC_App/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,6 +16,7 @@ namespace First_ASP_NET_MVC_App
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +26,15 @@ namespace First_ASP_NET_MVC_App
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(route =>
+            {
+                route.MapRoute(
+                    name: "default",
+                    template: "{controller=Home/{action=Index}/{id?}");
+            });
 
             app.Run(async (context) =>
             {
