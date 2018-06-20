718a2a92196f7bbaca892ea830bd35f661222a43
paulritzman <ritzman.paul@gmail.com>
2018-06-19 23:55:03 -0700

    Added logic for creating list of PersonsOfTheYear; Added cshtml and css files

--- First_ASP_NET_MVC_App/First_ASP_NET_MVC_App/Controllers/HomeController.cs
+++ First_ASP_NET_MVC_App/First_ASP_NET_MVC_App/Controllers/HomeController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using First_ASP_NET_MVC_App.Models;
+using First_ASP_NET_MVC_App.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace First_ASP_NET_MVC_App.Controllers
@@ -16,15 +18,23 @@ namespace First_ASP_NET_MVC_App.Controllers
         }
 
         // Page to send years input by the user back to the Controller for processing
-        //[HttpPost]
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
+        [HttpPost]
+        public IActionResult Index(PersonsOfTheYear PersonsOfTheYear)
+        {
+            return 
+        }
 
         // Page to display the "Person of the year", sorted to only include user specified years
-        public IActionResult Results()
+        public IActionResult Results(int startYear, int endYear)
         {
+            // logic to send data to View
+
+            ResultsViewModel rvm = new ResultsViewModel
+            {
+                StartYear = startYear,
+                EndYear = endYear
+            };
+
             return View();
         }
     }
--- First_ASP_NET_MVC_App/First_ASP_NET_MVC_App/Models/PersonsOfTheYear.cs
+++ First_ASP_NET_MVC_App/First_ASP_NET_MVC_App/Models/PersonsOfTheYear.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,5 +17,38 @@ namespace First_ASP_NET_MVC_App.Models
         public string Title { get; set; }
         public string Category { get; set; }
         public string Context { get; set; }
+
+        public PersonsOfTheYear()
+        {
+
+        }
+
+        public List<PersonsOfTheYear> GetPersons(int begYear, int endYear)
+        {
+            List<PersonsOfTheYear> people = new List<PersonsOfTheYear>;
+            string path = Environment.CurrentDirectory;
+            string newPath = Path.GetFullPath(Path.Combine(path, @"wwwroot\personOfTheYear.csv");
+            string[] myFile = File.ReadAllLines(newPath);
+
+            for (int i = 1; i < myFile.Length; i++)
+            {
+                string[] fields = myFile[i].Split(',');
+                people.Add(new PersonsOfTheYear
+                {
+                    Year = Convert.ToInt32(fields[0]),
+                    Honor = fields[1],
+                    Name = fields[2],
+                    Country = fields[3],
+                    Birth_Year = (fields[4] == "") ? 0 : Convert.ToInt32(fields[4]),
+                    DeathYear = (fields[5] == "") ? 0 : Convert.ToInt32(fields[5]),
+                    Title = fields[6],
+                    Category = fields[7],
+                    Context = fields[8]
+                });
+            }
+
+            List<PersonsOfTheYear> listOfPeople = people.Where(p => (p.Year >= begYear) && (p.Year <= endYear)).ToList();
+            return listOfPeople;
+        }
     }
 }
